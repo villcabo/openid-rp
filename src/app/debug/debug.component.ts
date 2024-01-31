56e04c89fa2dd0c4e267d0fa7e6694aea1e04f2c
Bismarck Villca <bismarck.villca@gmail.com>
2024-01-30 20:39:27 -0400

    feat: new view home and debug
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- src/app/debug/debug.component.ts
+++ src/app/debug/debug.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-debug',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './debug.component.html',
   styles: ``
 })
 export class DebugComponent implements OnInit {
 
   public queryParamList: Array<{ key: string, value: string }> = [];
-  public codeQuery!: {key: string, value: string};
+  public codeQuery!: { key: string, value: string };
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(queryParam => {
@@ -22,12 +23,26 @@ export class DebugComponent implements OnInit {
       Object.keys(queryParam).forEach(key => {
         const value = queryParam[key];
         if (key === 'code') {
-          this.codeQuery = {key: key, value: value};
-          this.queryParamList.push({key: key, value: value});
+          this.codeQuery = { key: key, value: value };
+          this.queryParamList.push({ key: key, value: value });
         } else {
-          this.queryParamList.push({key: key, value: value});
+          this.queryParamList.push({ key: key, value: value });
         }
       });
     })
+  }
+
+  onLogout() {
+    this.http.get('/jans-auth/restv1/end_session', { observe: 'response', responseType: 'text' })
+      .subscribe({
+        next: (val => {
+          console.info('End session success');
+          this.router.navigate([''])
+            .then(value => console.info('Navigated success: ' + value));
+        }),
+        error: (err => {
+          console.error('Failed to end session: ', err);
+        }),
+      });
   }
 }
